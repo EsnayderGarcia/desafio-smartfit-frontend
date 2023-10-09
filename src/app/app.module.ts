c1fe4bc7dc7c020991bcb98f2887430a4bceec43
EsnayderGarcia <scriptk.18@gmail.com>
2023-10-09 19:04:00 -0300

    feat: criação do formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HorarioFormComponent } from './components/horario-form/horario-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HorarioFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/horario-form/horario-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horario-form',
+  templateUrl: './horario-form.component.html',
+  styleUrls: ['./horario-form.component.scss']
+})
+export class HorarioFormComponent implements OnInit {
+  periodoEscolhido = '';
+  mostrarUnidadesFechadas = false;
+  resultados = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+ limparForm() {
+    this.periodoEscolhido = '';
+    this.mostrarUnidadesFechadas = false;
+ }
+}
